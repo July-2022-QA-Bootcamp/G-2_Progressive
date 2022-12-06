7c9a18000817b79c5c2ba9c67991a8f5660fb307
fargabuanik <fargabanik@gmail.com>
2022-12-06 18:47:42 -0500

    updata

--- src/main/java/utils/data/HomeOwners.java
+++ src/main/java/utils/data/HomeOwners.java
@@ -1,5 +1,21 @@
 package utils.data;
 
+
 public class HomeOwners {
+	private String zipCode;
+	private String  firstName;
+	private String lastName;
+	private String dob;
+	private String city;
+	private String state;
+	
+	public HomeOwners(String zipCode, String firstName, String lastName, String dob, String cityString, String State) {
+		this.zipCode = zipCode;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.city=
+		
+	}
 
 }
--- src/test/java/homeOwnerTest/Test_With_HomeOwnerData.java
+++ src/test/java/homeOwnerTest/Test_With_HomeOwnerData.java
@@ -1,10 +1,11 @@
 package homeOwnerTest;
 
 import base.BaseClass;
-import utils.data.AutoData;
 import utils.data.HomeOwners;
 
 public class Test_With_HomeOwnerData extends BaseClass {
+	
+	HomeOwners homeOwners = new HomeOwners("11210","Ali","khan","01/20/1960" ,"Brooklyn", "NY");
 	
 
 }
