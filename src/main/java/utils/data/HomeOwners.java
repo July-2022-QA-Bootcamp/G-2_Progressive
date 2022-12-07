7ade4ab2be9b249a33f544a25365d2853ae15264
fargabuanik <fargabanik@gmail.com>
2022-12-06 22:43:05 -0500

    updata

--- src/main/java/utils/data/HomeOwners.java
+++ src/main/java/utils/data/HomeOwners.java
@@ -8,14 +8,37 @@ public class HomeOwners {
 	private String dob;
 	private String city;
 	private String state;
-	
-	public HomeOwners(String zipCode, String firstName, String lastName, String dob, String cityString, String State) {
+
+	public HomeOwners(String zipCode, String firstName, String lastName, String dob, String city, String State, String state) {
 		this.zipCode = zipCode;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.dob = dob;
-		this.city=
-		
+		this.city = city;
+		this.state = state; 
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public String getCity() {
+		return city;
+	}
+	public String getState() {
+		return state;
 	}
 
 }
--- src/test/java/homeOwnerTest/Test_With_HomeOwnerData.java
+++ src/test/java/homeOwnerTest/Test_With_HomeOwnerData.java
@@ -1,11 +1,17 @@
 package homeOwnerTest;
 
+import org.testng.annotations.Test;
+
 import base.BaseClass;
 import utils.data.HomeOwners;
 
 public class Test_With_HomeOwnerData extends BaseClass {
 	
-	HomeOwners homeOwners = new HomeOwners("11210","Ali","khan","01/20/1960" ,"Brooklyn", "NY");
-	
+	HomeOwners homeOwners = new HomeOwners("11210","1030 Ocean ave","Ali","khan","01/20/1960" ,"Brooklyn", "NY");
+
+	@Test
+	public void HomeOwnerData () {
+		
+	}
 
 }
